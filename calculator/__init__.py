145587a7c5545974fab7fb3293ad5db52f5b50eb
yo49njit <yo49@njit.edu>
2025-06-10 23:50:09 -0400

    hw3 init

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,8 +1,58 @@
 """
 Calculator module containing basic arithmetic operations.
 """
+class Calculation:
+    """calculation class"""
+    def __init__(self, a, b, operation, result):
+        self.a = a
+        self.b = b
+        self.operation = operation
+        self.result = result
 
-def add(a, b):
-    """Returns the sum of a and b."""
-    return a + b
+    @staticmethod
+    def add(a, b):
+        """Returns the sum of a and b."""
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        """Returns the difference of a and b."""
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        """Returns the multiplication of a and b."""
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        """Returns the division of a and b."""
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
+        return a / b
+
+    @classmethod
+    def create(cls, a, b, operation_func):
+        result = operation_func(a, b)
+        return cls(a, b, operation_func.__name__, result)
+
+    def get_result(self):
+        """Returns results."""
+        return self.operation(self.a, self.b)
+        
+class CalculationHistory:
+    history = []
+
+    @classmethod
+    def add(cls, calculation):
+        cls.history.append(calculation)
+
+    @classmethod
+    def get_history(cls):
+        return cls.history
+
+    @classmethod
+    def clear_history(cls):
+        cls.history.clear()
+
 
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,8 +2,17 @@
 Unit tests for the calculator module.
 """
 
-from calculator import add
+from calculator import Calculation
 
 def test_addition():
     """Test the add() function."""
-    assert add(2, 2) == 4
+    assert Calculation.add(2, 2) == 4
+def test_subtraction():
+    """Test the subtract() function."""
+    assert Calculation.subtract(2, 2) == 0
+def test_multiply():
+    """Test the multiply() function."""
+    assert Calculation.multiply(2, 4) == 8
+def test_divide():
+    """Test the divide() function."""
+    assert Calculation.divide(2, 2) == 1
